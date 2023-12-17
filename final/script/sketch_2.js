1adea85f393a3e795d4b53b558d3db068daca111
lun2ee <128441642+lun2ee@users.noreply.github.com>
2023-12-18 02:14:43 +0900

    real_final_2_1218

--- final/script/sketch_2.js
+++ final/script/sketch_2.js
@@ -7,14 +7,8 @@ function setup() {
 
   document.body.appendChild(canvas);
 
-  //방위각에 대한 루프
   for (let azimNum = 1; azimNum < 8; azimNum++) {
-    //경사각에 대한 루프
     for (let zeniNum = 1; zeniNum < 8; zeniNum++) {
-      //Uni 클래스의 새로운 인스턴스를 생성하고 배열에 추가한다.
-      //azimuth 값은 (TAU/16)* azimNum 으로 설정되고,
-      //zenith 값은 (TAU/8)*zeniNum 으로 설정된다.
-      //나머지 매개 변수는 고정된 값인 300 과 50이다.
       unis.push(new Uni((TAU / 16) * azimNum, (TAU / 10) * zeniNum, 300, 20));
     }
   }
